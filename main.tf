2a07765ce4248727e7366282206df8f79bf7c284
Mechman27 <drbedfo@yahoo.com>
2024-03-29 08:29:36 -0600

    Updated Script to just install Docker

--- main.tf
+++ main.tf
@@ -27,20 +27,14 @@ resource "google_compute_instance" "web-server" {
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
-    sleep 10
+    
     # Add the repository to Apt sources:
     echo \
         'deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
         $(. /etc/os-release && echo '$VERSION_CODENAME') stable' | \
         sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    
-    sudo apt-get update
-    sleep 10
+        
+    sudo apt-get update;
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-    sleep 30
-    sudo docker build https://github.com/dockersamples/node-bulletin-board.git#master:bulletin-board-app
-    sleep 30
-    dockerimage =$(docker images -q)
-    sudo docker run -p 80:8080 $dockerimage
   SCRIPT
 }
